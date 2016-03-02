26ee5eefd3c68255c16c7f21c0af67731d264ef0
Joseph <ygorcahet@gmail.com>
2016-03-01 21:02:50 -0300

    exemplo de array

---
+++ execicios_php/arrays.php
@@ -0,0 +1,23 @@
+<?php
+
+	// criando e pecorrendo um lista
+
+	$lista = ["item1", "item2", "item3"];
+
+	foreach ($lista as $key => $value) {
+
+		echo $value;
+		
+	}
+
+	$grade = array("segunda-feira" => "programação" , "terça-feira" => "banco de dados" );
+
+	
+
+	foreach ($grade as $key => $valor) {
+
+		echo " " . $valor;
+		
+	}
+
+?>
