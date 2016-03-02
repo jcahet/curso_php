cda3d717df8eeaea8e59fcf4cf88b7f65eaac702
Joseph <ygorcahet@gmail.com>
2016-03-01 21:39:27 -0300

    exemplo de funções/ usando include da função

---
+++ execicios_php/calculadora.php
@@ -0,0 +1,12 @@
+<?php
+
+	include('functions.php');
+
+	$n1 = 10;
+	$n2 = 15;
+
+	$resut = somar($n1, $n2);
+
+	echo $resut;
+
+?>
---
+++ execicios_php/functions.php
@@ -0,0 +1,17 @@
+<?php
+
+	$numero = 10;
+	$numero2 = 30;
+
+	function somar($valor1 , $valor2){
+
+		$resultado = $valor1 + $valor2;
+
+		return $resultado;
+	}
+
+	$a = somar($numero , $numero2);
+
+	echo $a;
+
+?>
